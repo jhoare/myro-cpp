cdd6b72c8df9f4f4f0eb1dd911fbbdb5a36b4207
John Hoare <john@johnami.com>
2010-08-05 14:30:07 -0700

    So its spelled Gray in america, not grey.

--- headers/VideoStream.h
+++ headers/VideoStream.h
@@ -28,7 +28,7 @@ class VideoStream {
 
     public:
     /// The possible color modes for the VideoStream
-    enum color_mode { GREY=0, COLOR=1, BLOB=2 };
+    enum color_mode { GRAY=0, COLOR=1, BLOB=2 };
 
     /**
      * @param scrib - A pointer to a scribbler robot instance.
