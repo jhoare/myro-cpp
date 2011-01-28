48a5a6ebecdd370e29c17d68f1236f349fa4da38
John Hoare <john@johnami.com>
2011-01-27 21:49:55 -0500

    Cleaned up the ImageWindow object, and fixed a memory leak.

--- src/ImageWindow.cpp
+++ src/ImageWindow.cpp
@@ -9,30 +9,16 @@
 #include <iostream>
 #include <cstring>
 
-class Fl_Thread : public Threaded{
-    public:
-        Fl_Thread(ImageWindow * window);
-        //boost::mutex& setup, boost::condition& setup_notify);
-        //void start();
-        virtual void run();
-        //void stop();
-    private:
-        //Fl_Window * window;
-        ImageWindow * window;
-        //boost::mutex& setup;
-        //boost::condition& setup_notify;
-};
-
 ImageWindow::ImageWindow(int width, int height, char * title) 
 : Fl_Window(width, height, title), 
     image(NULL),
     exclusive(new boost::mutex()),
     m(NULL),
     cond(NULL)
-    //fl_thread(new Fl_Thread(this))
 { 
-    // Start the Fl_Thread display thread.
-    //fl_thread->start();
+    unsigned char temp[width*height*3];
+    std::memset(temp,255,width*height*3);
+    image = new Fl_RGB_Image(temp,width,height);
 }
 
 
@@ -50,9 +36,7 @@ ImageWindow::ImageWindow(int x, int y, int width,
 }
 
 ImageWindow::~ImageWindow(){
-    // Close and clean up our display thread
-    //fl_thread->stop();
-    //delete fl_thread;
+    delete exclusive;
     if (image) delete image;
 }
 
@@ -60,7 +44,10 @@ void ImageWindow::loadImageSource(unsigned char * data, int width, int height) {
     boost::mutex::scoped_lock l(*exclusive);
     if(image)
         delete image;
-    image = new Fl_RGB_Image((const unsigned char*)data, width, height);
+    if(color_mode)
+        image = new Fl_RGB_Image((const unsigned char*)data, width, height);
+    else
+        image = new Fl_RGB_Image((const unsigned char*)data, width, height,1);
 
 }
 
@@ -111,36 +98,3 @@ void ImageWindow::refresh() {
     this->redraw();
 }
 
-Fl_Thread::Fl_Thread(ImageWindow* window) : Threaded(), window(window)
-{}
-
-// FL Thread
-void Fl_Thread::run(){
-    {
-        //boost::mutex::scoped_lock l(setup);
-        this->window->show();
-        //std::cerr << "Fl_Thread::run() - started" << std::endl;
-        //setup_notify.notify_one();
-    }
-
-    while(!this->stopRequested){
-        //std::cerr << "Fl_Thread::run()" << std::endl;
-        if( this->window->visible() ) {
-            if(!Fl::check())
-                break;
-        }
-        else if( !Fl::wait() )
-            break;
-        //printf("start_stream() : imageWindow->redraw()\n");
-        //this->imageWindow->redraw();
-        this->window->refresh();
-        usleep(50000); // Sleep for .05 seconds, giving a 20hz refresh rate
-        //boost::thread::yield();
-    }
-    this->window->hide();
-    //this->imageWindow->hide();
-
-    //std::cerr << "Fl_Thread::run() - ending" << std::endl;
-
-    return;
-}
